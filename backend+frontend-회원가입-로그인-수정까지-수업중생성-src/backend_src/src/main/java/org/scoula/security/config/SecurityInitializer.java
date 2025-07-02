9151b8ac4c9343d0d9efbe61d8460d43dc6442ce
JAVA FullStack수업Git <47075040+newbackjava@users.noreply.github.com>
2025-07-02 14:38:08 +0900

    Update SecurityInitializer.java

--- backend+frontend-회원가입-로그인-수정까지-수업중생성-src/backend_src/src/main/java/org/scoula/security/config/SecurityInitializer.java
+++ backend+frontend-회원가입-로그인-수정까지-수업중생성-src/backend_src/src/main/java/org/scoula/security/config/SecurityInitializer.java
@@ -3,4 +3,17 @@ package org.scoula.security.config;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+      // 문자셋 필터
+    private CharacterEncodingFilter encodingFilter() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+        return encodingFilter;
+    }
+
+    @Override
+    protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+        insertFilters(servletContext, encodingFilter(), new MultipartFilter());
+    }
 }
