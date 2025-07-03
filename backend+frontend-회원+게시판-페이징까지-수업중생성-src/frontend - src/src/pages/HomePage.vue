4402315819e221b7cb3785deac27c569ba8854da
JAVA FullStack수업Git <47075040+newbackjava@users.noreply.github.com>
2025-07-03 13:11:13 +0900

    backend+frontend-회원+게시판-페이징까지-수업중생성-src

---
+++ backend+frontend-회원+게시판-페이징까지-수업중생성-src/frontend - src/src/pages/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import startImage from '@/assets/images/start.png';
+</script>
+
+<template>
+  <h1>KB 6기 파이팅!</h1>
+  <hr />
+  <img :src="startImage" alt="Start Image" width="700" height="500" />
+</template>
+
+<style scoped></style>
