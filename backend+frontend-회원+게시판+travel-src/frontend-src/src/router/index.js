e204c4a751179ead36ccbfc473e9e76d02d6759d
JAVA FullStack수업Git <47075040+newbackjava@users.noreply.github.com>
2025-07-07 17:03:42 +0900

    backend+frontend-회원+게시판+travel-src

---
+++ backend+frontend-회원+게시판+travel-src/frontend-src/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '../pages/HomePage.vue';
+import authRouters from './auth';
+import boardRouters from './board';
+import travelRouters from './travel';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage,
+    },
+    ...authRouters,
+    ...boardRouters,
+    ...travelRouters
+  ],
+});
+
+export default router;
